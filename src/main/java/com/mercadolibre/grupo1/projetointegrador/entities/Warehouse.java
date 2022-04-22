734691d0bf857c47cb2c1c378886c12bc4409e79
Ederson Rodrigues Araujo <ederodrigues@br0c02fq3y8md6m.ml.com>
2022-04-22 16:09:10 -0300

    entity warehouse - done

--- src/main/java/com/mercadolibre/grupo1/projetointegrador/entities/Warehouse.java
+++ src/main/java/com/mercadolibre/grupo1/projetointegrador/entities/Warehouse.java
@@ -1,4 +1,20 @@
 package com.mercadolibre.grupo1.projetointegrador.entities;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String address;
+    private String name;
+
 }
