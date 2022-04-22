06ef63e12e530510ec83f07da9e2f0a2f0a35815
Ederson Rodrigues Araujo <ederodrigues@br0c02fq3y8md6m.ml.com>
2022-04-22 16:26:41 -0300

    entity section - done

--- src/main/java/com/mercadolibre/grupo1/projetointegrador/entities/Section.java
+++ src/main/java/com/mercadolibre/grupo1/projetointegrador/entities/Section.java
@@ -1,4 +1,18 @@
 package com.mercadolibre.grupo1.projetointegrador.entities;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class Section {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Double capacity;
+
+    @ManyToOne
+    private Warehouse warehouse;
 }
