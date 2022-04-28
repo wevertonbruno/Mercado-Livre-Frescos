dd87c3e9302617269f1d6ec3fa048c917ab5148e
Jefferson <jefferson.botelho@mercadolivre.com>
2022-04-28 12:35:33 -0300

    perf: reaproveitamento de metodo para retorno de Excecao caso id nao encontrado

--- src/main/java/com/mercadolibre/grupo1/projetointegrador/services/PurchaseOrderServiceImpl.java
+++ src/main/java/com/mercadolibre/grupo1/projetointegrador/services/PurchaseOrderServiceImpl.java
@@ -3,6 +3,7 @@ package com.mercadolibre.grupo1.projetointegrador.services;
 import com.mercadolibre.grupo1.projetointegrador.dtos.ProductDTO;
 import com.mercadolibre.grupo1.projetointegrador.dtos.PurchaseOrderStatusDTO;
 import com.mercadolibre.grupo1.projetointegrador.entities.PurchaseOrder;
+import com.mercadolibre.grupo1.projetointegrador.entities.enums.OrderStatus;
 import com.mercadolibre.grupo1.projetointegrador.repositories.PurchaseOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ public class PurchaseOrderServiceImpl implements PurchaseOrderService {
     @Autowired
     private PurchaseOrderRepository purchaseOrderRepository;
 
+
     @Override
     public List<ProductDTO> allProductsList() {
         return null;
@@ -26,6 +28,7 @@ public class PurchaseOrderServiceImpl implements PurchaseOrderService {
         return null;
     }
 
+
     @Override
     public PurchaseOrder calcFinal(PurchaseOrder purchaseOrder) {
         return null;
@@ -33,32 +36,29 @@ public class PurchaseOrderServiceImpl implements PurchaseOrderService {
 
 
     /**
-     * a funcao showProductsInOrders ira retornar todos os produtos em um carrinho pelo id do carrinho
-     * a funcao editExistentOrder ira atualizar o status de um pedido
      *
      * @author  Jefferson Botelho
      * @since   2022-03-25
      *
      */
 
+    // a funcao showProductsInOrders ira retornar todos os produtos em um carrinho pelo id do carrinho
     @Override
     public PurchaseOrder showProductsInOrders(Long id) {
         PurchaseOrder order = purchaseOrderRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("TestCase nao encontrado!"));
+                .orElseThrow(() -> new RuntimeException("Pedido nao encontrado"));
 
         return order;
     }
 
+    // a funcao editExistentOrder ira atualizar o status de um pedido
     @Override
     public PurchaseOrderStatusDTO editExistentOrder(Long id, PurchaseOrderStatusDTO status) {
 
-        PurchaseOrder purchaseOrder = purchaseOrderRepository.findById(id).map(ordersMap -> {
-            ordersMap.setOrderStatus(status.getStatus());
-
-            return ordersMap;
-        }).orElseThrow(() -> new RuntimeException("Pedido nao encontrado"));
-
-        purchaseOrderRepository.save(purchaseOrder);
+        // reutilizando o metodo showProductsInOrders, caso o id nao seja encontrado sera retornado a Excecao do metodo
+        PurchaseOrder findId = showProductsInOrders(id);
+        findId.setOrderStatus(status.getStatus());
+        purchaseOrderRepository.save(findId);
 
         return new PurchaseOrderStatusDTO(status.getStatus());
     }
